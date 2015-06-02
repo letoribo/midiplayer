40ec21f7bc906b7ba0c6ce8681cce397ff1de575
letoribo <lawegard@yandex.ru>
2015-06-02 18:17:03 +0300

    Query unique records

--- urls_list.php
+++ urls_list.php
@@ -1,10 +1,10 @@
 <?php
 /* Connection string */
 $db = new PDO('mysql:host=localhost;dbname=linx', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    $sql = "SELECT * FROM urls";
+    $sql = "SELECT DISTINCT url FROM urls";
     $query = $db->prepare( $sql );
     $query->execute();
-    $results = $query->fetchAll( PDO::FETCH_UNIQUE );
+    $results = $query->fetchAll( PDO::FETCH_ASSOC );
     //get the number of the results found
     $num = $query->rowCount();
     echo $num; 
